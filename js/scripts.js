4c3d3505b64544651077de73b6eed86e8dafff1b
Luke Philips and Ryan Back <luke@>
2016-10-17 12:02:22 -0700

    simplified floating zeros function

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 
 // User input numbers
-// var number1 = parseFloat(prompt("Enter a number:"));
-// var number2 = parseFloat(prompt("Enter another number:"));
+var number1 = parseFloat(prompt("Enter a number:"));
+var number2 = parseFloat(prompt("Enter another number:"));
 
 //custom user input
-var weight = parseFloat(prompt("Enter your weight in pounds:"));
-var heightFeet = parseFloat(prompt("Enter your height in feet (you will add inches next):"));
-var heightInches = parseFloat(prompt("Enter your height in inches: "));
+// var weight = parseFloat(prompt("Enter your weight in pounds:"));
+// var heightFeet = parseFloat(prompt("Enter your height in feet (you will add inches next):"));
+// var heightInches = parseFloat(prompt("Enter your height in inches: "));
 
 // backend basic math operations
-// var add = function(number1, number2) {
-// 	return number1 + number2;
-// };
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// }
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-// var modulo = function(number1, number2) {
-// 	return number1 % number2;
-// };
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+var modulo = function(number1, number2) {
+	return number1 % number2;
+};
 
 //custom math functions
 var heightInInches = function(heightFeet, heightInches) {
@@ -35,6 +35,9 @@ var bmi = function (weight, heightInInches) {
 
 
 // frontend content to display
+var display = function(calculation) {
+	return Number(calculation.toFixed(8));
+};
 
 // var sum = Number(add(number1, number2).toFixed(8));
 // var difference = Number(subtract(number1, number2).toFixed(8));
@@ -42,18 +45,17 @@ var bmi = function (weight, heightInInches) {
 // var quotient = Number(divide(number1, number2).toFixed(8));
 // var remainder = Number(modulo(number1, number2).toFixed(8));
 
-// var fixedQuotient = quotient.toFixed(3)
-// var noTrailing = Number(fixedQuotient);
-// var testNoTrailing = Number(quotient.toFixed)
-// alert(typeof(noTrailing) + " " + noTrailing);
-
 //frontend display message
 
-// alert("The sum of " + number1 + " and " + number2 + " is " + sum);
-// alert("The difference between " + number1 + " and " + number2 + " is " + difference);
-// alert("The product of " + number1 + " and " + number2 + " is " + product);
-// alert("The quotient of " + number1 + " and " + number2 + " is " + quotient);
-// alert("The remainder of " + number1 + " and " + number2 + " is " + remainder);
+alert("The sum of " + number1 + " and " + number2 + " is " + display(add(number1, number2)));
+
+alert("The difference between " + number1 + " and " + number2 + " is " + display(subtract(number1, number2)));
+
+alert("The product of " + number1 + " and " + number2 + " is " + display(multiply(number1, number2)));
+
+alert("The quotient of " + number1 + " and " + number2 + " is " + display(divide(number1, number2)));
+
+alert("The remainder of " + number1 + " and " + number2 + " is " + display(modulo(number1, number2)));
 
 //custom display message
-alert("bmi is: " + Number(bmi(weight, heightInInches).toFixed(3)));
+// alert("bmi is: " + Number(bmi(weight, heightInInches).toFixed(3)));
